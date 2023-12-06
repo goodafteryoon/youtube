1abc607b829ad54d958101780582925ed8b39eca
yunna <homer_j@hanmail.net>
2023-12-06 15:58:19 +0900

    style : 검색 헤더 스타일 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,14 +19,20 @@ const Header = () => {
   useEffect(() => setText(keyword ? keyword : ''), [keyword]);
 
   return (
-    <header>
-      <Link to='/'>
-        <BsYoutube />
-        <h1 className='text-3xl'>Youtube</h1>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand' />
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleChange} value={text} />
-        <button>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+        <input
+          className='w-7/12 p-2 ouline-none bg-black text-gray-50'
+          type='text'
+          onChange={handleChange}
+          value={text}
+          placeholder='Search...'
+        />
+        <button className='bg-zinc-600 px-4'>
           <BsSearch />
         </button>
       </form>

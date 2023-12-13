868722cdb434f1bf3abefcfe706f1ea2bb952342
yunna <homer_j@hanmail.net>
2023-12-13 09:37:18 +0900

    add : video 섬네일, time 추가

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,11 +1,26 @@
-import { Item } from '../models/video/popular';
+import { format, register } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko';
 
+import { Item } from '../models/video/popular';
+import { formatAgo } from '../utill/date';
+
+register('ko', koLocale);
 interface VideoCardProps {
   video: Item;
 }
 
 const VideoCard = ({ video }: VideoCardProps) => {
-  return <p>{video.snippet.title}</p>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt, 'ko')}</p>
+      </div>
+    </li>
+  );
 };
 
 export default VideoCard;
